4bd79243449d3a9bab4613f11bfdbb26bb90b772
mattia_morriale <mattia.morriale1@gmail.com>
2024-04-02 16:42:35 +0200

    mod

--- index.php
+++ index.php
@@ -21,20 +21,22 @@ require 'db.php';
 </head>
 <body>
     
-    <ul>
-        <?php
+    
+    <?php
 
-        foreach($movies as $movie) {
-            echo "
-                <li>
-                    ". $movie->title . ", " . $movie->date . "<br>
-                    " . $movie->director?->getDirectorMovie() . "
-                </li>
-            ";
-        }
+    foreach($movies as $movie) {
+        echo "
+        <div class='card' style='width: 18rem;'>
+            <div class='card-body'>
+                <h5 class='card-title'>". $movie->title . ", " . $movie->date . "</h5>
+                <h6 class='card-subtitle mb-2 text-body-secondary'>" . $movie->director?->getDirectorMovie() . "</h6>
+                <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        ";
+    }
 
-        ?>
-    </ul>
+    ?>
 
     <!-- bootscript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
