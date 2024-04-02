1a43a1dbd81d28a872023a751c59b4ecaf892c1a
mattia_morriale <mattia.morriale1@gmail.com>
2024-04-02 17:25:34 +0200

    completed exercise

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require 'db.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP-OOP</title>
+    <title>Star Wars Movies</title>
 
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -19,13 +19,14 @@ require 'db.php';
 </head>
 <body data-bs-theme='dark'>
     
-    <div class="container-fluid d-flex justify-content-center">
-        <div class="my-box pt-5 ">
+    <div class="container-fluid d-flex align-items-center justify-content-center mt-5 pt-5   flex-column ">
+        <div class="my-box pt-5 mt-5">
+
             <?php
     
             foreach($movies as $movie) {
                 echo "
-                <div class='card my-card'>
+                <div class='card my-card border-warning '>
                     <div class='card-body'>
                         <h5 class='card-title'>". $movie->title . ", " . $movie->date . "</h5>
                         <h6 class='card-subtitle mb-2 text-body-secondary'>" . $movie->director?->getDirectorMovie() . "</h6>
